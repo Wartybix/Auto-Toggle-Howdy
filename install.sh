a5196c44dac3418c3bfbc6264c9ccd26e6899cf0
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-03-11 18:21:28 +0000

    Added systemd service that disables howdy on login screen

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ sudo cp enable-howdy.sh disable-howdy.sh /usr/local/bin/
 echo "$username ALL=(root) NOPASSWD: /usr/local/bin/enable-howdy.sh" | sudo tee /etc/sudoers.d/enable-howdy
 echo "$username ALL=(root) NOPASSWD: /usr/local/bin/disable-howdy.sh" | sudo tee /etc/sudoers.d/disable-howdy
 
+printf "[Service]\nExecStartPre=+-/bin/bash -c \"SUDO_USER=$username /usr/bin/howdy disable 1\"" | sudo tee /etc/systemd/system/systemd-logind.service.d/override.conf
+
 echo "Installed 😃"
